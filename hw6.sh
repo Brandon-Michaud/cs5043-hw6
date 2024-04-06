0f0d8bb70b7dc79bc1411522a72c9710afe568b6
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-05 19:24:52 -0500

    gru test

--- hw6.sh
+++ hw6.sh
@@ -18,4 +18,4 @@ conda activate tf
 module load cuDNN/8.9.2.26-CUDA-12.2.0
 
 
-python hw6_base.py -vv @exp.txt @oscer.txt @rnn.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --dataset /home/fagg/datasets/pfam
+python hw6_base.py -vv @exp.txt @oscer.txt @gru.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --dataset /home/fagg/datasets/pfam
