3a67e4bcd50e4336ceee457a89910b97b1c35b81
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-06 12:08:39 -0500

    n_tokens

--- hw6_base.py
+++ hw6_base.py
@@ -244,6 +244,7 @@ def execute_exp(args=None, multi_gpus=False):
                                   rotation=args.rotation,
                                   version='B')
     n_tokens = data_set_dict['n_tokens']
+    print(f'n_tokens: {n_tokens}')
     n_classes = data_set_dict['n_classes']
 
     ds_train, ds_validation, ds_testing = create_tf_datasets(dat=data_set_dict,
