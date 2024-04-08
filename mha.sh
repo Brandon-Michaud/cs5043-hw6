6a7ea4284a906bec3a0aff44d4940b8a9721b2b1
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-08 15:00:12 -0500

    mha full fo

--- mha.sh
+++ mha.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+#SBATCH --gres=gpu:1
 #SBATCH --partition=disc_dual_a100_students,gpu,gpu_a100
 #SBATCH --cpus-per-task=64
 #SBATCH --mem=80G
@@ -10,7 +11,7 @@
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504319/cs5043-hw6
-#SBATCH --array=0
+#SBATCH --array=0-4
 
 . /home/fagg/tf_setup.sh
 conda activate tf
