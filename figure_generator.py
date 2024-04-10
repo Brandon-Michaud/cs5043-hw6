a692528487946751199c3bf6b2bbdf1152297eeb
Brandon Michaud <michaudbrandond@gmail.com>
2024-04-10 11:09:17 -0500

    figure generators

--- figure_generator.py
+++ figure_generator.py
@@ -5,29 +5,47 @@ import pickle
 
 def scatter_accuracies():
     # Load accuracies from results files
-    rnn_test_accuracy = np.empty(5)
-    cnn_test_accuracy = np.empty(5)
+    gru_test_accuracy = np.empty(5)
+    mha_test_accuracy = np.empty(5)
     for r in range(5):
-        with open(f'results/rnn_rnns_32_16_8_ract_tanh_dense_128_64_32_dact_elu_gc_0.001000_lrate_0.000100_rot_{r}_'
-                  f'results.pkl', "rb") as fp:
+        with open(f'results/gru_grus_200_100_gact_tanh_dense_128_64_dact_elu_gc_0.001000_md_0.001000_lrate_0.000100_'
+                  f'rot_{r}_results.pkl', "rb") as fp:
             results = pickle.load(fp)
-            rnn_test_accuracy[r] = results['predict_testing_eval'][1]
-        with open(f'results/cnn_filters_32_64_128_256_ksizes_128_64_32_16_pool_2_pad_valid_cact_elu_dense_128_64_32_'
-                  f'dact_elu_gc_0.010000_lrate_0.000100_rot_{r}_results.pkl', "rb") as fp:
+            gru_test_accuracy[r] = results['predict_testing_eval'][1]
+        with open(f'results/mha_nheads_8_kdims_8_dense_128_64_dact_elu_gc_0.001000_md_0.001000_lrate_0.000100_'
+                  f'rot_{r}_results.pkl', "rb") as fp:
             results = pickle.load(fp)
-            cnn_test_accuracy[r] = results['predict_testing_eval'][1]
+            mha_test_accuracy[r] = results['predict_testing_eval'][1]
 
     # Make scatter plot of accuracies
     fig = plt.figure()
     for i in range(5):
-        plt.scatter(rnn_test_accuracy[i], cnn_test_accuracy[i], label=f'Rotation {i}')
+        plt.scatter(gru_test_accuracy[i], mha_test_accuracy[i], label=f'Rotation {i}')
     plt.legend()
-    plt.xlabel('RNN Accuracy')
-    plt.ylabel('CNN Accuracy')
+    plt.xlabel('GRU Accuracy')
+    plt.ylabel('MHA Accuracy')
     plt.title('Model Accuracy')
     plt.axis('equal')
     fig.savefig('figures/model_accuracies.png')
 
 
+def scatter_epochs():
+    # Load epochs. Hard-coded because I forgot to save to pkl file. Values taken from wandb
+    gru_epochs = [322, 398, 424, 389, 172]
+    mha_epochs = [67, 28, 21, 32, 41]
+
+    # Make scatter plot of epochs
+    fig = plt.figure()
+    for i in range(5):
+        plt.scatter(gru_epochs[i], mha_epochs[i], label=f'Rotation {i}')
+    plt.legend()
+    plt.xlabel('GRU Epochs')
+    plt.ylabel('MHA Epochs')
+    plt.title('Model Epochs')
+    plt.axis('equal')
+    fig.savefig('figures/model_epochs.png')
+
+
 if __name__ == '__main__':
     scatter_accuracies()
+    scatter_epochs()
--- figure_generator.sh
+++ figure_generator.sh
@@ -3,13 +3,13 @@
 #SBATCH --partition=debug
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=1G
-#SBATCH --output=outputs/hw5_%j_stdout.txt
-#SBATCH --error=outputs/hw5_%j_stderr.txt
+#SBATCH --output=outputs/hw6_%j_stdout.txt
+#SBATCH --error=outputs/hw6_%j_stderr.txt
 #SBATCH --time=00:30:00
-#SBATCH --job-name=hw5
+#SBATCH --job-name=hw6
 #SBATCH --mail-user=brandondmichaud@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --chdir=/home/cs504319/cs5043-hw5
+#SBATCH --chdir=/home/cs504319/cs5043-hw6
 
 . /home/fagg/tf_setup.sh
 conda activate dnn_2024_02
